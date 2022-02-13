e075aa5280e756d6758af7e1f6f883291c2f9557
IrynaDar <darmitsira@gmail.com>
2022-02-13 12:54:24 +0200

    footer upd

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,14 @@
 (() => {
-    const mobileMenu = document.querySelector('burgerMenu');
-    console.log('mobileMenu');
-});
+const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+};
+
+refs.openMenuBtn.addEventListener("click", toggleMenu);
+refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+}
+})();
